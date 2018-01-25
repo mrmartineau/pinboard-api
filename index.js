3b77b9ae7d5a76fb059a30c21ed29e0f5a72a194
Zander Martineau <zander.martineau@nimbletank.com>
2018-01-25 12:51:25 +0000

    make the service properly CORS enabled

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const axios = require('axios')
 const { send } = require('micro')
+const microCors = require('micro-cors')
+const cors = microCors({ allowMethods: ['GET'] })
 const DOMAIN = 'http://feeds.pinboard.in/'
 
-module.exports = async function(req, res) {
+const handler = async function(req, res) {
   const params = req.url
   const path = `${DOMAIN}${params}`
   const response = await axios(path)
-  res.setHeader('Access-Control-Allow-Origin', '*')
   send(res, 200, response.data)
 }
+
+module.exports = cors(handler)
